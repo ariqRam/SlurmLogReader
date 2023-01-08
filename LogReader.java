a04bd54fc1389b109d6804a406a38734aef137d2
ramdhanyA <ariqathallah38@gmail.com>
2023-01-09 00:39:02 +0800

    Categorized the type of jobs and stored it to job_types.txt

--- LogReader.java
+++ LogReader.java
@@ -1,18 +1,54 @@
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class LogReader {
     public static void main(String[] args) {
 
+        countTypeOfJob("extracted_log");
+    }
+
+    public static void countTypeOfJob(String filename) {
         try {
-            FileInputStream fis = new FileInputStream("extracted_log");
-            Scanner sc = new Scanner(fis);
-    
-            while(sc.hasNextLine()) {
-                System.out.println(sc.nextLine());
+            FileInputStream fis = new FileInputStream(filename);
+            Scanner logSc = new Scanner(fis);
+
+            
+            outerloop:
+            while(logSc.hasNextLine()) {
+                String[] splittedLine = logSc.nextLine().split(" ");
+                String newType =  splittedLine[1];
+                FileInputStream jobTypeFile = new FileInputStream("job_types.txt");
+                Scanner jobTypeScanner = new Scanner(jobTypeFile);
+
+
+                FileOutputStream fos = new FileOutputStream("job_types.txt", true);
+                PrintWriter printer = new PrintWriter(fos);
+
+                if(!jobTypeScanner.hasNextLine()) {
+                    printer.println(newType);
+                                   
+                    jobTypeScanner.close();
+                    printer.close();
+                    continue;
+                }
+
+                while(jobTypeScanner.hasNextLine()) {
+                    String nextLine = jobTypeScanner.nextLine();
+                    if(newType.compareTo(nextLine) == 0) {
+                        // System.out.printf("%s == %s already exists in job_types.txt\n", newType, nextLine);
+                        continue outerloop;
+                    }
+                }
+                System.out.println("writing " + newType);
+                printer.println(newType);               
+                jobTypeScanner.close();
+                printer.close();
             }
-            sc.close();
+            logSc.close();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
